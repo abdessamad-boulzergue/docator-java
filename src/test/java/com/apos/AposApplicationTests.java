bd12ba86f225e32b06c8b3762c50d12b84ca2ce5
abdessamad <abdessamad.boulzergue@docaposte.fr>
2020-08-08 11:25:37 +0200

    set Junit user controller

--- src/test/java/com/apos/AposApplicationTests.java
+++ src/test/java/com/apos/AposApplicationTests.java
@@ -1,13 +1,39 @@
 package com.apos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.CoreMatchers.is;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.apos.rest.repo.UserRepo;
+
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = AposApplication.class
+		)
+@AutoConfigureMockMvc
 class AposApplicationTests {
 
+	@Autowired
+    private MockMvc mvc;
+ 
+    @Autowired
+    private UserRepo repository;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/users/all")
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(MockMvcResultMatchers.status().isOk())
+			      .andExpect(MockMvcResultMatchers.content()
+			      .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			      .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", is("bob")));
 	}
 
 }
